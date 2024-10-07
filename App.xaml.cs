044938225d11f229dd55f89e76aac6a1f7a8abc4
soyticz <143929580+soyticz@users.noreply.github.com>
2024-10-07 06:14:27 +0000

    asd

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace wpf1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
